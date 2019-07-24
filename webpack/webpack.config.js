7f6cfb5e88d299921a4205f6a6e3dffa32aecafc
18445166828 <1406225846@qq.com>
2019-07-24 14:55:28 +0800

    webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,14 +1,27 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
-  entry:{
+  entry:{//入口
     index:"./src/index.js"
   },
-  output:{
+  output:{//出口
     path:path.resolve(__dirname,"dist"),
     filename:'[name].js'
   },
-  plugins:[
+  module:{
+    rules:[
+       {
+        test:/\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      }
+    ]
+  },
+  plugins:[//配置
+    new ExtractTextPlugin("css/main.css"),    
     new HtmlWebpackPlugin({  
       minify:{
         removeAttributeQuotes:true
@@ -19,7 +32,7 @@ module.exports = {
     })
   ],
   devServer:{
-    contentBase:path.resolve(__dirname,"dist"),
+    contentBase:path.resolve(__dirname,"dist"),//获取当前路径，指向dist
     host:"localhost",
     compress:true,
     port:"8081"
