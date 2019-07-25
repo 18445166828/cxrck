2e347b0d26f68fa3a0a16116ce22dfa0edc6fa66
18445166828 <1406225846@qq.com>
2019-07-25 11:24:23 +0800

    webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const glob=require("glob");
+const PurifyCSSPlugin=require('purifycss-webpack');
 module.exports = {
   entry:{//入口
     index:"./src/index.js"
@@ -34,13 +36,18 @@ module.exports = {
         test:/\.scss/,
         use:ExtractTextPlugin.extract({
           fallback: "style-loader",
-          use: ["css-loader","sass-loader"]     
+          use: [{loader:"css-loader",
+                 options:{importLoaders:1}
+        },'postcss-loader']     
         })
       }
     ]
   },
   plugins:[//配置
-    new ExtractTextPlugin("css/main.css"),    
+    new ExtractTextPlugin("css/main.css"), 
+    new PurifyCSSPlugin({
+      paths:glob.sync(path.join(__dirname,'src/*.html'))
+    }),   
     new HtmlWebpackPlugin({  
       minify:{
         removeAttributeQuotes:true
