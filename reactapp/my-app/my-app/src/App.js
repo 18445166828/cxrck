59b499d9d27cd1a49f416409abeecb0aab54398c
18445166828 <1406225846@qq.com>
2019-08-09 10:39:56 +0800

    reactapp

--- reactapp/my-app/my-app/src/App.js
+++ reactapp/my-app/my-app/src/App.js
@@ -1,24 +1,29 @@
+// exact 路由严格匹配模式
+// switch	仅渲染与当前位置匹配的第一个子元素
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,  
+  Link,
+  Switch,
+  Redirect
+ } from "react-router-dom";
+ import Test1 from "./component/Test1"
+ import Test2 from "./component/Test2"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Router>
+       <Link to="/">test1</Link>
+       <Link to="/test2">test2</Link>
+       <Switch>
+       <Route path="/test2" component={Test2}></Route>
+       <Route path="/" component={Test1}></Route>
+       </Switch>
+     </Router>
     </div>
   );
 }
---
+++ reactapp/my-app/my-app/src/component/Test1.js
@@ -0,0 +1,12 @@
+// rcc快捷键
+import React, { Component } from 'react'
+
+export default class Test1 extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Test1</h1>
+            </div>
+        )
+    }
+}
